5c9ab22d4266bd60f357a320ea0fbacb18d6cd67
YusufLisawi <yusufisawi@gmail.com>
2022-11-16 23:01:10 +0100

    mongodb

--- MyPass.py
+++ MyPass.py
@@ -40,7 +40,6 @@ import pymongo
 # To create a database in MongoDB, 
 # start by creating a MongoClient object, 
 # then specify a connection URL with the correct ip address 
-# and the name of the database you want to create.
 db = pymongo.MongoClient("mongodb://localhost:27017/")
 # Creating the Database "passwords"
 # MongoDB will create the database if it does not exist.
